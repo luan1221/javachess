11a5c207db1b279250a992f8ce0a242e05c69556
luan1221 <lssa@cin.ufpe.br>
2022-04-10 23:17:40 -0300

    board and piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,26 @@
+package boardgame;
+
+public abstract class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return true;
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        return false;
+    }
+}
