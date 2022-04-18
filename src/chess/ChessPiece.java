2425b21d8eb33323aa4488c9eceaf1e40a77066d
luan1221 <lssa@cin.ufpe.br>
2022-04-18 15:06:42 -0300

    beginning to implement possible moves

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,17 +4,13 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
     private Color color;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
-    }
-
-    public boolean[][] possibleMoves(Position position) {
-        return null;
     }
 
     public Color getColor() {
