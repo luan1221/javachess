d42ee1bab4d457fd63a8b0ce4b4e035f7d9fde48
luan1221 <lssa@cin.ufpe.br>
2022-04-20 18:21:27 -0300

    check UI

--- src/app/UI.java
+++ src/app/UI.java
@@ -55,6 +55,9 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+        if (chessMatch.isCheck()) {
+            System.out.println("CHECK!");
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
