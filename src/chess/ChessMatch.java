3e40dbd4cb903e3da573037296275f29fb694f0f
luan1221 <lssa@cin.ufpe.br>
2022-04-20 18:15:16 -0300

    check mechanic implemented

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -39,6 +39,14 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+
+        if (testCheck(currentPlayer)) {
+            undoMove(source, target, capturedPiece);
+            throw new ChessException("You can't put yourself in check.");
+        }
+
+        check = (testCheck(opponent(currentPlayer))) ? true : false;
+
         nextTurn();
         return (ChessPiece) capturedPiece;
     }
@@ -101,6 +109,18 @@ public class ChessMatch {
         throw new IllegalStateException("There is no " + color + "king on the board.");
     }
 
+    private boolean testCheck(Color color) {
+        Position kingPosition = king(color).getChessPosition().toPosition();
+        List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(p -> ((ChessPiece)p).getColor() == opponent(color)).collect(Collectors.toList());
+        for (Piece p : opponentPieces) {
+            boolean[][] possibleOpponentMoves = p.possibleMoves();
+            if (possibleOpponentMoves[kingPosition.getRow()][kingPosition.getColumn()]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
         piecesOnTheBoard.add(piece);
@@ -142,6 +162,10 @@ public class ChessMatch {
 
     public Color getCurrentPlayer() {
         return currentPlayer;
+    }
+
+    public boolean isCheck() {
+        return check;
     }
 
     public void setCurrentPlayer(Color currentPlayer) {
